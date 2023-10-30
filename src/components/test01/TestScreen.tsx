e24ca42f9a2cb09f22974e08e78a3868e5e36f8a
ktyoung <ktyoung0507@gmail.com>
2023-10-30 14:10:57 +0900

    feat: Implement TestScreen

--- src/components/test01/TestScreen.tsx
+++ src/components/test01/TestScreen.tsx
@@ -1,11 +1,84 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function TestScreen() {
+  const [isPlay, setIsPlay] = useState(false);
+  const [isOpenAnswer, setIsOpenAnswer] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlay(!isPlay);
+  };
+  const toggleOpenAnswer = () => {
+    setIsOpenAnswer(!isOpenAnswer);
+  };
+
   return (
     <div className="test-screen-wrapper">
-      <div className="test-contents">
-        <div className="play-sentence"></div>
-        <div className="view-sentence"></div>
+      <div className="answer-buttons">
+        <button>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/test/button_correct.png`}
+            alt="Correct answer button"
+          />
+        </button>
+        <button>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/test/button_wrong.png`}
+            alt="Wrong answer button"
+          />
+        </button>
       </div>
-      <div className="test-buttons"></div>
+      <div className="test-contents">
+        <Link to="">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/test/button_left.png`}
+            alt="Go to previous question"
+          />
+        </Link>
+        <div className="test-contents__bar">
+          <div
+            className={`play-sentence ${isPlay && "playing"}`}
+            onClick={togglePlay}
+          >
+            {isPlay ? (
+              <img
+                src={`${process.env.PUBLIC_URL}/images/test/pause.png`}
+                alt="Sentence listening pause icon"
+                style={{ marginRight: "20px" }}
+              />
+            ) : (
+              <img
+                src={`${process.env.PUBLIC_URL}/images/test/play_small.png`}
+                alt="Sentence listening icon"
+              />
+            )}
+
+            <p>문장 듣기</p>
+          </div>
+          <div className="view-sentence" onClick={toggleOpenAnswer}>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/test/check.png`}
+              alt="Sentence viewing icon"
+            />
+            <p>문장 보기</p>
+            {isOpenAnswer && (
+              <div className="view-sentence__accordion">
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/test/play_small.png`}
+                  alt="Sentence listening icon"
+                />
+                <p>테스트 텍스트입니다.</p>
+              </div>
+            )}
+          </div>
+        </div>
+        <Link to="">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/test/button_right.png`}
+            alt="Go to next question"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
