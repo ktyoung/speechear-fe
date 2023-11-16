54c3ca8fd37da8e0797741edd12fa888c8c09b53
ktyoung <ktyoung0507@gmail.com>
2023-11-16 10:54:41 +0900

    refactor(Login, Home): update design
    - 로그인 페이지: 폰트 크기 증가
    - 홈(메인) 페이지: 하단 설명 문구 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,37 +6,15 @@ export default function Home() {
       <div className="contents-main">
         <div className="main-title">
           <p className="select-function">듣기연습 선택</p>
-          <p className="select-function small-text">
-            실행할 듣기 연습 종류를 선택하세요.
-          </p>
+          <p className="select-function small-text">연습할 듣기 종류를 고르세요.</p>
         </div>
         <div className="functions-wrapper">
           <ul>
-            <FunctionList
-              _to="/training/part1"
-              _num="01"
-              _title={`소음 하\n문장 듣기`}
-            />
-            <FunctionList
-              _to="/training/part2"
-              _num="02"
-              _title={`짧은 이야기\n듣기`}
-            />
-            <FunctionList
-              _to="/training/part3"
-              _num="03"
-              _title={`긴 이야기\n듣기`}
-            />
-            <FunctionList
-              _to="/training/part4"
-              _num="04"
-              _title={`문장\n순서화 하기`}
-            />
-            <FunctionList
-              _to="/training/part5"
-              _num="05"
-              _title={`가로세로\n퀴즈`}
-            />
+            <FunctionList _to="/training/part1" _num="01" _title={`소음 하\n문장 듣기`} />
+            <FunctionList _to="/training/part2" _num="02" _title={`짧은 이야기\n듣기`} />
+            <FunctionList _to="/training/part3" _num="03" _title={`긴 이야기\n듣기`} />
+            <FunctionList _to="/training/part4" _num="04" _title={`문장\n순서화 하기`} />
+            <FunctionList _to="/training/part5" _num="05" _title={`가로세로\n퀴즈`} />
           </ul>
         </div>
       </div>
