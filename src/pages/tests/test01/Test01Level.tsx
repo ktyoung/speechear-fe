aa78a57f2b7a4e0d7fcb1e57548676f3c74027ed
ktyoung <ktyoung0507@gmail.com>
2023-12-26 11:30:42 +0900

    Desing Test01Level Page

--- src/pages/tests/test01/Test01Level.tsx
+++ src/pages/tests/test01/Test01Level.tsx
@@ -1,17 +1,17 @@
-import { useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import useAxios, { API_URL, IRequestType } from "@hooks/useAxios";
-import TestList from "@components/tests/TestList";
 import { useEffect, useState } from "react";
+import Snb from "@components/common/Snb";
+
+const TOTAL_CARDS = 100;
+const CARDS_PER_PAGE = 10;
 
 export default function Test01Level() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentCards, setCurrentCards] = useState<number[]>([]);
   const { level, page } = useParams<{ level: string; page?: string }>();
   const [request, setRequest] = useState<IRequestType>();
   const result = useAxios(request);
-
-  // test loading
-  // useEffect(() => {
-  //   console.log("result?.loading ", result?.loading);
-  // }, [result?.loading]);
 
   useEffect(() => {
     console.log("page ", page);
@@ -24,21 +24,97 @@ export default function Test01Level() {
     result.fetchData();
   }, [page]);
 
+  const onPageChange = (pageNumber: number): void => {
+    setCurrentPage(pageNumber);
+  };
+
+  useEffect(() => {
+    const startNumber: number = (currentPage - 1) * CARDS_PER_PAGE + 1;
+    const newCards: number[] = Array.from(
+      { length: CARDS_PER_PAGE },
+      (_, index) => startNumber + index
+    );
+    setCurrentCards(newCards);
+  }, [currentPage]);
+
   return (
-    <div className="contents-wrapper main">
-      <div className="contents-main">
-        <div className="main-title menu no-margin-bottom">
-          <p className="select-function menu-title menu-title-color">
-            <span className="function-number menu-number-color">01</span> 소음
-            하 문장듣기
-          </p>
+    <div className="main-wrapper">
+      <div className="main-contents home test">
+        <div className="snb">
+          <Snb />
         </div>
-        <TestList
-          partNum={1}
-          data={result.data}
-          _to={`/training/part1/${level}/${page}`}
+        <div className="main-contents__column">
+          <p className="mb pb">듣기 연습 선택</p>
+          <div className="main-select-wrapper visible">
+            <p className="font-light">실행할 듣기 연습 종류를 선택하세요.</p>
+            <div className="status-card-wrapper">
+              {currentCards.map((number) => (
+                <StatusCard key={number} number={number} />
+              ))}
+            </div>
+            <Pagination
+              currentPage={currentPage}
+              totalPages={TOTAL_CARDS / CARDS_PER_PAGE}
+              onPageChange={onPageChange}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface StatusCardProps {
+  number: number;
+}
+
+function StatusCard({ number }: StatusCardProps) {
+  const [progress, setProgress] = useState(50);
+  const location = useLocation();
+
+  const testScreenPath = `${location.pathname}/${number}`;
+
+  return (
+    <Link to={testScreenPath} className="status-card">
+      <p className="status-card__number">{number}</p>
+      <div className="status-card__progress-bar">
+        <div
+          className="status-card__progress-fill"
+          style={{ width: `${progress}%` }}
+        ></div>
+      </div>
+      <div className="status-card__percentage">{progress}%</div>
+      <div className="status-card__complete">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/icons/icon_progress_check_white.png`}
+          alt="White Check Icon"
         />
       </div>
+    </Link>
+  );
+}
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (pageNumber: number) => void;
+}
+function Pagination({ currentPage, totalPages, onPageChange }: PaginationProps) {
+  return (
+    <div className="pagination-wrapper">
+      <ul>
+        {[...Array(totalPages)].map((_, index) => {
+          const number = index + 1;
+          return (
+            <li
+              key={number}
+              className={`page-item ${number === currentPage ? "active" : ""}`}
+            >
+              <button onClick={() => onPageChange(number)}>{number}</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
