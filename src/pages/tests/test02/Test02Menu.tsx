e16ad49a7a457c3b4637f5ddf47fa037b230506d
ktyoung <ktyoung0507@gmail.com>
2024-01-03 11:07:23 +0900

    Design Test02Menu

--- src/pages/tests/test02/Test02Menu.tsx
+++ src/pages/tests/test02/Test02Menu.tsx
@@ -1,40 +1,44 @@
-import Menus from "../../../components/tests/Menus";
+import TestCategory from "@components/TestCategory";
+import Snb from "@components/common/Snb";
 
 export default function Test02Menu() {
+  const testCategories = [
+    {
+      icon: "location",
+      children: "지역",
+    },
+    {
+      icon: "culture",
+      children: "우리문화",
+    },
+    {
+      icon: "food",
+      children: "음식",
+    },
+    {
+      icon: "etc",
+      children: "기타",
+    },
+  ];
+
   return (
-    <div className="contents-wrapper main">
-      <div className="contents-main">
-        <div className="main-title menu">
-          <p className="select-function menu-title menu-title-color">
-            <span className="function-number menu-number-color">02</span>
-            짧은 이야기 듣기
-          </p>
+    <div className="main-wrapper">
+      <div className="main-contents home test">
+        <div className="snb">
+          <Snb />
         </div>
-        <div className="functions-wrapper menus">
-          <Menus
-            _className="menu02-style"
-            _to="location/1"
-            _difficulty="지역"
-            _title=""
-          />
-          <Menus
-            _className="menu02-style"
-            _to="culture/1"
-            _difficulty="우리문화"
-            _title=""
-          />
-          <Menus
-            _className="menu02-style"
-            _to="food/1"
-            _difficulty="음식"
-            _title=""
-          />
-          <Menus
-            _className="menu02-style"
-            _to="etc/1"
-            _difficulty="기타"
-            _title=""
-          />
+        <div className="main-contents__column">
+          <p className="mb pb">듣기 연습 선택</p>
+          <div className="main-select-wrapper visible">
+            <p className="font-bold">듣고 싶은 이야기를 골라주세요</p>
+            <div className="test-category-container">
+              {testCategories.map((items, i) => {
+                return (
+                  <TestCategory key={i} icon={items.icon} children={items.children} />
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
