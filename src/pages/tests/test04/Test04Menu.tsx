69b1071f19ef044a7e4c18641bab4fd1f420964a
ktyoung <ktyoung0507@gmail.com>
2024-01-10 12:01:58 +0900

    Design Test04Menu

--- src/pages/tests/test04/Test04Menu.tsx
+++ src/pages/tests/test04/Test04Menu.tsx
@@ -1,36 +1,66 @@
-import Menus from "../../../components/tests/Menus";
+import TestCategory from "@components/TestCategory";
+import Snb from "@components/common/Snb";
+import { Link } from "react-router-dom";
 
 export default function Test04Menu() {
+  const testCategories = [
+    {
+      title: "3문장 세트",
+      detail: "문장 세개를 순서에 맞게 선택",
+      to: "word3",
+    },
+    {
+      title: "4문장 세트",
+      detail: "문장 네개를 순서에 맞게 선택",
+      to: "word4",
+    },
+    {
+      title: "5문장 세트",
+      detail: "문장 다섯개를 순서에 맞게 선택",
+      to: "word5",
+    },
+  ];
+
   return (
-    <div className="contents-wrapper main">
-      <div className="contents-main">
-        <div className="main-title menu">
-          <p className="select-function menu-title menu-title-color">
-            <span className="function-number menu-number-color">04</span>
-            문장 순서화 하기
-          </p>
+    <div className="main-wrapper">
+      <div className="main-contents home test">
+        <div className="snb">
+          <Snb />
         </div>
-        <div className="functions-wrapper menus">
-          <Menus
-            _className="card-wide"
-            _to="word3/1"
-            _difficulty="3문장 세트"
-            _title={"문장 세개를\n순서에 맞게 선택"}
-          />
-          <Menus
-            _className="card-wide"
-            _to="word4/1"
-            _difficulty="4문장 세트"
-            _title={"문장 네개를\n순서에 맞게 선택"}
-          />
-          <Menus
-            _className="card-wide"
-            _to="word5/1"
-            _difficulty="5문장 세트"
-            _title={"문장 다섯개를\n순서에 맞게 선택"}
-          />
+        <div className="main-contents__column">
+          <p className="mb pb">문장 순서화 하기</p>
+          <div className="main-select-wrapper visible">
+            {/* <p className="font-bold">듣고 싶은 이야기를 골라주세요</p> */}
+            {testCategories.map((items, i) => {
+              return (
+                <TestMenuCard
+                  key={i}
+                  title={items.title}
+                  detail={items.detail}
+                  to={items.to}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+interface TestMenuCardProps {
+  title: string;
+  detail: string;
+  to: string;
+}
+function TestMenuCard({ title, detail, to }: TestMenuCardProps) {
+  return (
+    <div className="test-card-container">
+      <p className="card-title">{title}</p>
+      <p className="card-detail">{detail}</p>
+      <Link to={`/training/part4/${to}`} className="card-link">
+        이동하기
+      </Link>
     </div>
   );
 }
