53e10f09fd22f5f7105bac7f17bdf2eae8e4cea2
ktyoung <ktyoung0507@gmail.com>
2024-01-15 14:55:49 +0900

    feat: design Test05Menu

--- src/pages/tests/test05/Test05Menu.tsx
+++ src/pages/tests/test05/Test05Menu.tsx
@@ -1,52 +1,78 @@
+import Snb from "@components/common/Snb";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Test05Menu() {
-  const [pageNumber, setPageNumber] = useState(1);
-  const numberArray = Array.from({ length: 100 }, (_, i) => i + 1);
-  const numbersToDisplay = numberArray.slice(
-    (pageNumber - 1) * 50,
-    pageNumber * 50
-  );
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const _handlePageNumber = (newPageNumber: number) => {
-    setPageNumber(newPageNumber);
+  // 이전 또는 다음 페이지로 이동하기 위한 로직
+  const handleLeftArrowClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
   };
+  const handleRightArrowClick = () => {
+    if (currentPage < 2) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+  //
+
+  console.log(currentPage);
 
   return (
-    <div className="contents-wrapper main">
-      <div className="contents-main">
-        <div className="main-title menu no-margin-bottom">
-          <p className="select-function menu-title menu-title-color">
-            <span className="function-number menu-number-color">05</span>
-            가로세로퀴즈
-          </p>
+    <div className="main-wrapper">
+      <div className="main-contents home test">
+        <div className="snb">
+          <Snb />
         </div>
-        <div className="functions-wrapper menus flex-column">
-          <div className="menu-header">
-            <p>오늘 풀어볼 퀴즈를 고르세요.</p>
-            <ul className="page-btn-wrapper">
-              <li className={`page-btn ${pageNumber === 1 ? "active" : ""}`}>
-                <button onClick={() => _handlePageNumber(1)}>1</button>
-              </li>
-              <li className={`page-btn ${pageNumber === 2 ? "active" : ""}`}>
-                <button onClick={() => _handlePageNumber(2)}>2</button>
-              </li>
-            </ul>
+        <div className="main-contents__column">
+          <p className="mb pb">가로세로 퀴즈</p>
+          <div className="main-select-wrapper visible relative">
+            <div className="pagination-arrows">
+              <button onClick={handleLeftArrowClick}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/icons/icon_arrow_left.png`}
+                  alt="Left Arrow Icon"
+                  style={{
+                    opacity: currentPage === 1 ? 0.5 : 1,
+                    cursor: currentPage === 1 ? "default" : "pointer",
+                  }}
+                />
+              </button>
+              <button onClick={handleRightArrowClick}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/images/icons/icon_arrow_right.png`}
+                  alt="Right Arrow Icon"
+                  style={{
+                    opacity: currentPage === 2 ? 0.5 : 1,
+                    cursor: currentPage === 2 ? "default" : "pointer",
+                  }}
+                />
+              </button>
+            </div>
+            <QuizLinks page={currentPage} />
           </div>
-          <ul className="grid grid-small">
-            {numbersToDisplay.map((number) => {
-              return (
-                <li key={number} className="test-card-small">
-                  <Link
-                    to={`/training/part5/${number}`}
-                  >{`퀴즈 ${number}`}</Link>
-                </li>
-              );
-            })}
-          </ul>
         </div>
       </div>
+    </div>
+  );
+}
+
+interface QuizLinksProps {
+  page: number;
+}
+function QuizLinks({ page }: QuizLinksProps) {
+  const start = (page - 1) * 50 + 1;
+  const end = page * 50;
+
+  return (
+    <div className="quiz-link-container">
+      {Array.from({ length: end - start + 1 }, (_, i) => i + start).map((quizNumber) => (
+        <Link key={quizNumber} to={`/training/part5/${quizNumber}`}>
+          퀴즈 <br /> {quizNumber}
+        </Link>
+      ))}
     </div>
   );
 }
