93c3c315584b0b6d09f331eb96af3be494fa6590
ktyoung <ktyoung0507@gmail.com>
2024-01-29 14:12:26 +0900

    feat(Test01Menu): 모바일 대응 반응형 디자인 적용

--- src/pages/tests/test01/Test01Menu.tsx
+++ src/pages/tests/test01/Test01Menu.tsx
@@ -2,18 +2,20 @@ import categories from "@datas/testCategories.json";
 
 import Snb from "@components/common/Snb";
 import DifficultyCard from "@components/tests/test01/DifficultyCard";
+import SwipeableHeaderTabs from "@components/common/SwipeableHeaderTabs";
 
 export default function Test01Menu() {
   const testCategories = categories["01"];
 
   return (
-    <div className="main-wrapper">
+    <div className="main-wrapper bg-gray">
       <div className="main-contents home test">
         <div className="snb">
           <Snb />
         </div>
         <div className="main-contents__column">
           <p>소음 하 문장 듣기</p>
+          <SwipeableHeaderTabs />
           <div className="main-select-wrapper">
             {testCategories.map((level) => (
               <DifficultyCard
