44a383ca56e66458e42d271c7826b2ff241515ad
ktyoung <ktyoung0507@gmail.com>
2024-01-30 13:34:56 +0900

    feat(Test02Menu): 모바일 대응 반응형 디자인 적용

--- src/pages/tests/test02/Test02Menu.tsx
+++ src/pages/tests/test02/Test02Menu.tsx
@@ -2,29 +2,37 @@ import categories from "@datas/testCategories.json";
 
 import TestCategory from "@components/TestCategory";
 import Snb from "@components/common/Snb";
+import SwipeableHeaderTabs from "@components/common/SwipeableHeaderTabs";
+import { Link } from "react-router-dom";
 
 export default function Test02Menu() {
   const testCategories = categories["02"];
 
   return (
-    <div className="main-wrapper">
+    <div className="main-wrapper bg-gray">
       <div className="main-contents home test">
         <div className="snb">
           <Snb />
         </div>
         <div className="main-contents__column">
           <p className="mb pb">짧은 이야기 듣기</p>
+          <SwipeableHeaderTabs />
           <div className="main-select-wrapper visible">
             <p className="font-bold">듣고 싶은 이야기를 골라주세요</p>
             <div className="test-category-container">
               {testCategories.map((items, i) => {
                 return (
-                  <TestCategory
-                    key={i}
-                    part="part2"
-                    icon={items.icon}
-                    children={items.children}
-                  />
+                  <>
+                    <TestCategory
+                      key={i}
+                      part="part2"
+                      icon={items.icon}
+                      children={items.children}
+                    />
+                    <Link className="link-sm" to={`/training/part2/${items.icon}/1`}>
+                      {items.children}
+                    </Link>
+                  </>
                 );
               })}
             </div>
