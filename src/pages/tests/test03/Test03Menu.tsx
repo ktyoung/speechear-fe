ac35020d6bbe98ed1faf71758397076316b34990
ktyoung <ktyoung0507@gmail.com>
2024-01-31 12:09:01 +0900

    feat(Test03Menu): 모바일 대응 반응형 디자인 적용

--- src/pages/tests/test03/Test03Menu.tsx
+++ src/pages/tests/test03/Test03Menu.tsx
@@ -2,29 +2,38 @@ import categories from "@datas/testCategories.json";
 
 import Snb from "@components/common/Snb";
 import TestCategory from "@components/TestCategory";
+import SwipeableHeaderTabs from "@components/common/SwipeableHeaderTabs";
+import { Link } from "react-router-dom";
+import React from "react";
 
 export default function Test03Menu() {
   const testCategories = categories["03"];
 
   return (
-    <div className="main-wrapper">
+    <div className="main-wrapper bg-gray">
       <div className="main-contents home test">
         <div className="snb">
           <Snb />
         </div>
         <div className="main-contents__column">
           <p className="mb pb">긴 이야기 듣기</p>
+          <SwipeableHeaderTabs />
           <div className="main-select-wrapper visible">
             <p className="font-bold">듣고 싶은 이야기를 골라주세요</p>
             <div className="test-category-container grid">
               {testCategories.map((items, i) => {
                 return (
-                  <TestCategory
-                    key={i}
-                    part="part3"
-                    icon={items.icon}
-                    children={items.children}
-                  />
+                  <React.Fragment key={i}>
+                    <TestCategory
+                      key={i}
+                      part="part3"
+                      icon={items.icon}
+                      children={items.children}
+                    />
+                    <Link className="link-sm" to={`/training/part3/${items.icon}/1`}>
+                      {items.children}
+                    </Link>
+                  </React.Fragment>
                 );
               })}
             </div>
