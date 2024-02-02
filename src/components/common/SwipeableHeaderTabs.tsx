2e4870c42119dd1ffe65443b2eb8fe53624db537
ktyoung <ktyoung0507@gmail.com>
2024-02-02 16:35:51 +0900

    refactor(SwipeableHeaderTabs): 컴포넌트 구조 개선

--- src/components/common/SwipeableHeaderTabs.tsx
+++ src/components/common/SwipeableHeaderTabs.tsx
@@ -5,30 +5,23 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import "swiper/css";
 
-export default function SwipeableHeaderTabs() {
+type TabDetail = {
+  title: string;
+  to: string;
+};
+
+type SwipeableHeaderTabsProps = {
+  tabsDetail: TabDetail[];
+};
+
+export default function SwipeableHeaderTabs({ tabsDetail }: SwipeableHeaderTabsProps) {
   const location = useLocation();
-  const swiperRef = useRef<SwiperCore | undefined>(undefined);
+  const swiperRef = useRef<SwiperCore | null>(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const tabsDetail = [
-    { title: "소음 하 문장듣기", to: "/training/part1" },
-    { title: "짧은 이야기 듣기", to: "/training/part2" },
-    { title: "긴 이야기 듣기", to: "/training/part3" },
-    { title: "문장 순서화 하기", to: "/training/part4" },
-    { title: "가로세로 퀴즈", to: "/training/part5" },
-  ];
-
-  // 초기 슬라이드 인덱스를 계산하는 함수
-  type TabDetail = {
-    title: string;
-    to: string;
-  };
   const findInitialSlideIndex = (pathname: string, tabsDetail: TabDetail[]): number => {
     return tabsDetail.findIndex((tab) => pathname.includes(tab.to));
   };
-  //
-
-  const initialSlideIndex = findInitialSlideIndex(location.pathname, tabsDetail);
 
   useEffect(() => {
     const tabIndex = findInitialSlideIndex(location.pathname, tabsDetail);
@@ -40,7 +33,7 @@ export default function SwipeableHeaderTabs() {
 
   return (
     <Swiper
-      initialSlide={initialSlideIndex}
+      initialSlide={findInitialSlideIndex(location.pathname, tabsDetail)}
       slidesPerView={"auto"}
       centeredSlides={true}
       spaceBetween={10}
@@ -50,22 +43,20 @@ export default function SwipeableHeaderTabs() {
       }}
       className="swipe-header-tabs-container"
     >
-      {tabsDetail.map((tab, i) => {
-        return (
-          <SwiperSlide key={i} style={{ width: "200px" }}>
-            <Link
-              to={tab.to}
-              className="header-tabs"
-              style={{
-                background: i === activeIndex ? "rgba(99, 180, 255, 0.10)" : "",
-                color: i === activeIndex ? "#4894FE" : "",
-              }}
-            >
-              {tab.title}
-            </Link>
-          </SwiperSlide>
-        );
-      })}
+      {tabsDetail.map((tab, index) => (
+        <SwiperSlide key={index} style={{ width: "200px" }}>
+          <Link
+            to={tab.to}
+            className="header-tabs"
+            style={{
+              background: index === activeIndex ? "rgba(99, 180, 255, 0.10)" : "",
+              color: index === activeIndex ? "#4894FE" : "",
+            }}
+          >
+            {tab.title}
+          </Link>
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 }
